a42a8488f24bfed4cf221ae4b65b665b73811e0b
victor247k <topgspineivictor@gmail.com>
2024-03-31 16:57:33 +0300

    get routes for login and sign up

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path')
+const fs = require('fs')
+
 
 app.get('/', (req, res)=> {
-    res.send("hello")
-})
+	res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+
+app.get('/login', (req, res)=> {
+	res.sendFile(path.join(__dirname, "./public/login.html"))
+});
+
+app.get('/signup', (req, res)=> {
+	res.sendFile(path.join(__dirname, "./public/signup.html"))
+});
 
 app.listen(port, () => {
     console.log('http://localhost:3000/')
-})
+});
