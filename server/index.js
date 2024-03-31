6c3910fb86acb2f5cbff43c009f235af506caaac
victor247k <topgspineivictor@gmail.com>
2024-03-31 17:38:43 +0300

    email and password validation for signup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 const path = require('path')
 const fs = require('fs')
 
+const utils = require('./utils.js')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=> {
 	res.sendFile(path.join(__dirname, "./public/index.html"))
@@ -15,6 +20,22 @@ app.get('/login', (req, res)=> {
 
 app.get('/signup', (req, res)=> {
 	res.sendFile(path.join(__dirname, "./public/signup.html"))
+});
+
+app.post('/signup', (req, res) => {
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!utils.ValidateEmail(email)) {
+        res.send("Invalid email <br> <a href=\"/signup\">Go Back!</a>");
+    }
+    if (password.length < 8) {
+        res.send("Password too short<br> <a href=\"/signup\">Go Back!</a>");
+    }
+    
+
+    res.redirect('/login');
 });
 
 app.listen(port, () => {
---
+++ server/utils.js
@@ -0,0 +1,4 @@
+exports.ValidateEmail = (email) => {
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return email.match(validRegex);
+}
