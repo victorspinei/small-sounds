1b0c57bb31c6a54311306da7396f26249ff68508
victor247k <topgspineivictor@gmail.com>
2024-04-10 20:25:41 +0300

    signup post route complete

--- server/index.js
+++ server/index.js
@@ -1,25 +1,41 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const port = 3000;
+
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+
+const bodyParser = require('body-parser');
+
 const path = require('path')
 const fs = require('fs')
+
+const sqlite3 = require('sqlite3').verbose();
+
+
+const db = new sqlite3.Database(path.join(__dirname, "./server.db"), (err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.message);
+    } else {
+        console.log('Connected to the database.');
+    }
+})
 
 const utils = require('./utils.js')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> {
-	res.sendFile(path.join(__dirname, "./public/index.html"))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get('/login', (req, res)=> {
-	res.sendFile(path.join(__dirname, "./public/login.html"))
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/login.html"));
 });
 
-app.get('/signup', (req, res)=> {
-	res.sendFile(path.join(__dirname, "./public/signup.html"))
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/signup.html"));
 });
 
 app.post('/signup', (req, res) => {
@@ -29,13 +45,42 @@ app.post('/signup', (req, res) => {
 
     if (!utils.ValidateEmail(email)) {
         res.send("Invalid email <br> <a href=\"/signup\">Go Back!</a>");
+        return;
     }
     if (password.length < 8) {
         res.send("Password too short<br> <a href=\"/signup\">Go Back!</a>");
+        return;
     }
-    
+    db.all('SELECT * FROM users WHERE username = ?', username, (err, rows) => {
+        if (err) {
+            console.error('Error selecting data:', err.message);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
 
-    res.redirect('/login');
+        // Check if any rows were returned
+        if (rows.length !== 0) {
+            res.send("Username already exists<br> <a href=\"/signup\">Go Back!</a>");
+        } else {
+            bcrypt.hash(password, saltRounds, (err, hash) => {
+                if (err) {
+                    console.error('Error generating hash:', err.message);
+                    res.status(500).send('Internal Server Error');
+                    return;
+                } else {
+                    db.run('INSERT INTO users (username, email, hash, joined) VALUES (?, ?, ?, ?)', [username, email, hash, (new Date()).toDateString()], (err) => {
+                        if(err) {
+                            console.error('Error inserting data:', err.message);
+                            res.status(500).send('Internal Server Error');
+                            return;
+                        }
+                        console.log('User registered successfully');
+                        res.redirect('/login')
+                    });
+                }
+            });
+        }
+    });
 });
 
 app.listen(port, () => {
