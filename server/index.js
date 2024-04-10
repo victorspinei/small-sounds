febea30bfd4b0dee2294703a39aedbcd3296d56d
victor247k <topgspineivictor@gmail.com>
2024-04-10 20:51:14 +0300

    login pretty much complete

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 const app = express();
 const port = 3000;
+app.use(cookieParser())
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true
+}))
 
 const bcrypt = require('bcrypt')
 const saltRounds = 10;
@@ -87,6 +95,67 @@ app.post('/signup', (req, res) => {
         }
     });
 });
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const trap = req.body.trap;
+
+    if (trap !== '') {
+        res.send("Extra input values <br> <a href=\"/signup\">Go Back!</a>");
+        return;
+    }
+    db.all('SELECT * FROM users WHERE username = ?', username, (err, rows) => {
+        if (err) {
+            console.error('Error selecting data:', err.message);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        // Check if any rows were returned
+        if (rows.length === 0) {
+            res.send("Username not found<br> <a href=\"/login\">Go Back!</a>");
+            return;
+        } else {
+            bcrypt.compare(password, rows[0].hash, (err, result)=> {
+                if (err) {
+                    console.log('Error comparing passwords:', err.message);
+                } else {
+                    if (result) {
+                        req.session.isLoggedIn = true;
+                        res.cookie('loggedIn', true, {maxAge: 900000, httpOnly: true });
+                        res.redirect('/')
+                    } else {
+                        res.send("Passwords do not match<br> <a href=\"/login\">Go Back!</a>");
+                        return;
+                    }
+                }
+            });
+        }
+    });
+
+});
+
+app.get('/logout', (req, res) => {
+    // Clear session
+    req.session.destroy();
+    // Clear login cookie
+    res.clearCookie('loggedIn');
+    res.redirect('/login'); // Redirect to login page
+});
+
+app.get('/example', (req, res) => {
+    if (req.session.isLoggedIn || req.cookies.loggedIn) {
+        // User is logged in
+        // Proceed with rendering the dashboard
+        res.render('example');
+    } else {
+        // User is not logged in
+        // Redirect to login page or show an error message
+        res.redirect('/login');
+    }
+});
+
 
 app.listen(port, () => {
     console.log('http://localhost:3000/')
